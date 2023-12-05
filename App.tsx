c30c74daa164510264fd4e4959a0af272c7d5dee
Ida Krech <krech.ida@gmail.com>
2023-12-05 12:29:39 +0100

    Endret bakgrunnsfarge i App.tsx

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4287f5', /** testing git */
     alignItems: 'center',
     justifyContent: 'center',
   },
