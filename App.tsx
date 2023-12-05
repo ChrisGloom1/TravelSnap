70e8521ee46584067f07b19cfc117c787a6a52b9
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2023-12-05 11:55:19 +0100

    Changed bg color

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
+    <View className='flex-1 items-center justify-center bg-slate-400'>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
