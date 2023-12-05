b7fcc7b3b63d6769a43e92c224353238585d27fa
Ida Krech <krech.ida@gmail.com>
2023-12-05 12:33:49 +0100

    Endret bakgrunnsfarge 2.0

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4287f5', /** testing git */
+    backgroundColor: '#8142f5', /** testing git */
     alignItems: 'center',
     justifyContent: 'center',
   },
