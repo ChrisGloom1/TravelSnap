e2835828fc6070efff2282861f543b8b721b8c1a
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2023-12-07 11:28:25 +0100

    merged camera to main

--- App.tsx
+++ App.tsx
@@ -2,10 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FeedImage from './src/components/FeedPost/FeedPost';
-import FeedPost from './src/components/FeedPost/FeedPost';
 import CameraComponent from './src/components/CameraComponent/CameraComponent';
-import { Camera } from 'expo-camera';
 
 const Stack = createNativeStackNavigator();
 
