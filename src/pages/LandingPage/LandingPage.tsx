2f98511ee3ab2ef5683b7284ba6396d987c4439e
Ida Krech <krech.ida@gmail.com>
2023-12-08 00:58:37 +0100

    Added empty remaining pages.

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import { Text, View, Button } from 'react-native'
+import React from 'react'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+
+export default function LandingPage() {
+  return (
+    <View>
+      <Text>LandingPage</Text>
+    </View>
+  )
+}
