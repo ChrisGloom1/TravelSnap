e089296451056128dc68c3ea8c2595af81fd9e82
Ida Krech <krech.ida@gmail.com>
2023-12-07 21:21:10 +0100

    Added Landing Page

--- App.tsx
+++ App.tsx
@@ -2,17 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CameraComponent from './src/components/CameraComponent/CameraComponent';
-import ProfileSetupPage from './src/pages/ProfileSetupPage/ProfileSetupPage';
-import RegisterPage from './src/pages/RegisterPage/RegisterPage';
-import LoginPage from './src/pages/LoginPage/LoginPage';
+import FeedImage from './src/components/FeedPost/FeedPost';
+import FeedPost from './src/components/FeedPost/FeedPost';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     // <NavigationContainer>
-      <ProfileSetupPage />
+      <FeedPost />
     // </NavigationContainer>
   );
 }
