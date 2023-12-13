796df6c8d3dcd4ea110d584dc7b76d72075c6b41
Ida Krech <krech.ida@gmail.com>
2023-12-13 13:01:19 +0100

    Tabs added for ExplorePage

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,17 +1,21 @@
-import { View, Text, Button, ScrollView } from 'react-native'
+import { View, Text, Button, ScrollView, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { QueryDocumentSnapshot, collection, doc, getDoc, getDocs, onSnapshot, or, orderBy, query, where } from 'firebase/firestore';
 import { db } from '../../../firebase';
 import FeedPost, { FeedPostProps } from '../../components/FeedPost/FeedPost';
 import * as Location from "expo-location";
 import MapView, { Marker } from 'react-native-maps';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 const ExplorePage = () => {
   const [posts, setPosts] = useState<FeedPostProps[]>([]);
   const [username, setUsername] = useState<string>("no username");
   const [profileImg, setProfileImg] = useState<string>("");
   const [locationName, setLocationName] = useState<string>("");
-  const [location, setLocation] = useState<Location.LocationObject>();
+  const [postLocation, setPostLocation] = useState<Location.LocationObject>(); 
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+
+  const Tab = createMaterialTopTabNavigator();
 
   useEffect(() => {
     
@@ -23,7 +27,7 @@ const ExplorePage = () => {
       }
 
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      setPostLocation(location);
 
       let reverseGeocode = await Location.reverseGeocodeAsync({
         latitude: location.coords.latitude,
@@ -31,40 +35,74 @@ const ExplorePage = () => {
       });
 
       if (reverseGeocode && reverseGeocode.length > 0) {
-        setLocationName(reverseGeocode[0]?.city || "Unknown Location");
+        setLocationName(reverseGeocode[0]?.country || "Unknown Location");
       
       
-      const unsubscribePosts = onSnapshot(
-        query(
-          collection(db, 'posts'), //(db, `posts/${userId}/userPosts`)
-          where("city", "==", "Kłodzko"),
-          orderBy("timestamp", "desc")
-        ),
-        (snapshot) => {
-          setPosts(
-            snapshot.docs.map((doc: QueryDocumentSnapshot) => {
-              const data = doc.data();
-              findLocationName(data.coords.latitude, data.coords.longitude);
-              fetchUserInfo(data.userID)
-              return {
-                postID: doc.id,
-                username: username,
-                userImage: profileImg,
-                image: data.image,
-                caption: data.caption,
-                timestamp: data.timestamp,
-                latitude: data.coords.latitude,
-                longitude: data.coords.longitude,
-                locationName: locationName,
-              };
-            })
-          );
-        }
-      );
-      unsubscribePosts()
+      
       }
     })();
   }, [locationName]);
+
+  const fetchPostsDescending = () => {
+    const unsubscribePosts = onSnapshot(
+      query(
+        collection(db, 'posts'), //(db, `posts/${userId}/userPosts`)
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc: QueryDocumentSnapshot) => {
+            const data = doc.data();
+            findLocationName(data.coords.latitude, data.coords.longitude);
+            fetchUserInfo(data.userID)
+            return {
+              postID: doc.id,
+              username: username,
+              userImage: profileImg,
+              image: data.image,
+              caption: data.caption,
+              timestamp: data.timestamp,
+              latitude: data.coords.latitude,
+              longitude: data.coords.longitude,
+              locationName: locationName,
+            };
+          })
+        );
+      }
+    );
+    unsubscribePosts();
+  }
+
+  const fetchPostsByCountry = (country: string) => {
+    const unsubscribePosts = onSnapshot(
+      query(
+        collection(db, 'posts'), //(db, `posts/${userId}/userPosts`)
+        where("country", "==", country),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc: QueryDocumentSnapshot) => {
+            const data = doc.data();
+            findLocationName(data.coords.latitude, data.coords.longitude);
+            fetchUserInfo(data.userID)
+            return {
+              postID: doc.id,
+              username: username,
+              userImage: profileImg,
+              image: data.image,
+              caption: data.caption,
+              timestamp: data.timestamp,
+              latitude: data.coords.latitude,
+              longitude: data.coords.longitude,
+              locationName: locationName,
+            };
+          })
+        );
+      }
+    );
+    unsubscribePosts()
+  }
 
   const searchPostsByKeyword = (keyword: string) => {
     const trimmedKeyword = keyword.trim().toLowerCase();
@@ -146,54 +184,154 @@ const ExplorePage = () => {
   }
 
   return (
-    
     <ScrollView>
-      <Text>{locationName}</Text>
-      {/* CREATE AN INPUT AND PASS THE INPUT'S VALUE TO THE FUNCTION IN ONPRESS IN THE BUTTON BELOW */}
-      <Button title="Search" onPress={() => searchPostsByKeyword("Kłodzko")}/>
-      {posts.length > 0 ? (
-        posts.map((post) => (
-          <View key={Math.random()}>
-            <FeedPost
-              key={post.postID}
-              postID={post.postID}
-              userImage={profileImg}
-              image={post.image}
-              caption={post.caption}
-              username={username}
-              latitude={post.latitude}
-              longitude={post.longitude}
-              timestamp={post.timestamp}
-              locationName={post.locationName}
-            />
-            <MapView
-              style={{ height: 200 }}
-              key={Math.random()}
-              initialRegion={{
-                latitude: post.latitude,
-                longitude: post.longitude,
-                latitudeDelta: 0.005,
-                longitudeDelta: 0.005,
-              }}
-            >
-              {post.latitude && post.longitude && (
-                <Marker
-                  coordinate={{
-                    latitude: post.latitude,
-                    longitude: post.longitude,
-                  }}
-                  title="Photo location"
-                  identifier="Photo location"
-                />
-              )}
-            </MapView>
-          </View>
-        ))
-      ) : (
-        <Text>Loading...</Text>
-      )}
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Newest"
+          component={() => {
+            return (
+              <View>
+                {posts.map((post) => (
+                  <View key={Math.random()}>
+                    <FeedPost
+                      key={post.postID}
+                      postID={post.postID}
+                      userImage={profileImg}
+                      image={post.image}
+                      caption={post.caption}
+                      username={username}
+                      latitude={post.latitude}
+                      longitude={post.longitude}
+                      timestamp={post.timestamp}
+                      locationName={post.locationName}
+                    />
+                    <MapView
+                      style={{ height: 200 }}
+                      key={Math.random()}
+                      initialRegion={{
+                        latitude: post.latitude,
+                        longitude: post.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005,
+                      }}
+                    >
+                      {post.latitude && post.longitude && (
+                        <Marker
+                          coordinate={{
+                            latitude: post.latitude,
+                            longitude: post.longitude,
+                          }}
+                          title="Photo location"
+                          identifier="Photo location"
+                        />
+                      )}
+                    </MapView>
+                  </View>
+                ))}
+              </View>
+            );
+          }}
+        />
+  
+        <Tab.Screen
+          name="City"
+          component={() => (
+            <View>
+              {posts.map((post) => (
+                <View key={Math.random()}>
+                  <FeedPost
+                    key={post.postID}
+                    postID={post.postID}
+                    userImage={profileImg}
+                    image={post.image}
+                    caption={post.caption}
+                    username={username}
+                    latitude={post.latitude}
+                    longitude={post.longitude}
+                    timestamp={post.timestamp}
+                    locationName={post.locationName}
+                  />
+                  <MapView
+                    style={{ height: 200 }}
+                    key={Math.random()}
+                    initialRegion={{
+                      latitude: post.latitude,
+                      longitude: post.longitude,
+                      latitudeDelta: 0.005,
+                      longitudeDelta: 0.005,
+                    }}
+                  >
+                    {post.latitude && post.longitude && (
+                      <Marker
+                        coordinate={{
+                          latitude: post.latitude,
+                          longitude: post.longitude,
+                        }}
+                        title="Photo location"
+                        identifier="Photo location"
+                      />
+                    )}
+                  </MapView>
+                </View>
+              ))}
+            </View>
+          )}
+        />
+  
+        <Tab.Screen
+          name="Search"
+          component={() => (
+            <View>
+              <TextInput
+                placeholder="Search posts..."
+                onChangeText={(text) => setSearchKeyword(text)}
+                value={searchKeyword}
+              />
+              <Button title="Search" onPress={() => searchPostsByKeyword(searchKeyword)} />
+              {posts.map((post) => (
+                <View key={Math.random()}>
+                  <FeedPost
+                    key={post.postID}
+                    postID={post.postID}
+                    userImage={profileImg}
+                    image={post.image}
+                    caption={post.caption}
+                    username={username}
+                    latitude={post.latitude}
+                    longitude={post.longitude}
+                    timestamp={post.timestamp}
+                    locationName={post.locationName}
+                  />
+                  <MapView
+                    style={{ height: 200 }}
+                    key={Math.random()}
+                    initialRegion={{
+                      latitude: post.latitude,
+                      longitude: post.longitude,
+                      latitudeDelta: 0.005,
+                      longitudeDelta: 0.005,
+                    }}
+                  >
+                    {post.latitude && post.longitude && (
+                      <Marker
+                        coordinate={{
+                          latitude: post.latitude,
+                          longitude: post.longitude,
+                        }}
+                        title="Photo location"
+                        identifier="Photo location"
+                      />
+                    )}
+                  </MapView>
+                </View>
+              ))}
+            </View>
+          )}
+        />
+      </Tab.Navigator>
     </ScrollView>
-  )
+  );
+  
 }
 
 export default ExplorePage;
