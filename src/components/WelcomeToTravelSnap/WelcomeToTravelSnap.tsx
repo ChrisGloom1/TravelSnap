04c6e2718cbdb0fe5b6e2c7c9277110a4dfa88ec
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2023-12-14 14:16:15 +0100

    justert logo for keyboard access ved login og register

--- src/components/WelcomeToTravelSnap/WelcomeToTravelSnap.tsx
+++ src/components/WelcomeToTravelSnap/WelcomeToTravelSnap.tsx
@@ -4,11 +4,11 @@ const WelcomeToTravelSnap = () => {
   return (
     <View 
       // className="flex-1 justify-center items-center"
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+      style={{display: "flex", justifyContent: 'center', alignItems: 'center'}}
     >
       <Image 
         source={require('../../../assets/liggendelogo.png')}
-        style={{ width: 300, height: 50, marginBottom: 64 }}
+        style={{ width: 300, height: 50, marginBottom: 32 }}
         resizeMode="contain"
       />
     </View>
